1bec6fba8ebb547970594a4a387e36152bcbaf25
JackieYuan <nguyentuyenfc96@gmail.com>
2023-08-16 01:26:05 +0700

    Add roles,users

---
+++ CoffeeShop/CoffeeShop.Data/Entities/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeShop.Data.Entities
+{
+    public class Roles
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
